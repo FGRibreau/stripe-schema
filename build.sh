dda6cd4c70b8a123f4a4dbbd2de16490435bb102
Francois-Guillaume Ribreau <github@fgribreau.com>
2019-10-09 19:23:18 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
+jqn -r markdown-table 'map(x => "* [" + x.name + "](#"+x.name+")") | join("\n")' < /tmp/stripe.json
 jqn -r markdown-table 'map(x => "## " + x.name + "\n\n" + markdownTable(x.columns.map(y => [y.name, y.type]))  ) | join("\n\n")' < /tmp/stripe.json
